a71c218e0f50c780fe8382cf7c6051d89698d1aa
Hoàng Công Anh <anhhc@boxme.asia>
2024-08-19 19:50:11 +0700

    edit workflow

--- deploy.sh
+++ deploy.sh
@@ -26,5 +26,13 @@ docker run -d --name $CONTAINER_NAME -p $PORT:5000 $IMAGE_NAME
 
 if [ "$CURRENT_IMAGE_ID" != "$NEW_IMAGE_ID" ]; then
     echo "\n------------------ REMOVE OLD IMAGE ------------------ "
+    echo "docker rmi $CURRENT_IMAGE_ID"
     docker rmi $CURRENT_IMAGE_ID
 fi
+
+echo "Branch name: $BRANCH_NAME"
+echo "Image name: $IMAGE_NAME"
+echo "Container name: $CONTAINER_NAME"
+echo "Port: $PORT"
+echo "Current image id: $CURRENT_IMAGE_ID"
+echo "New image id: $NEW_IMAGE_ID"
